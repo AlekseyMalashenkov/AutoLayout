670f83b11482dd7e6b7113fce4668e24ba3ce777
AlekseyMalashenkov <malashenkov93av@gmail.com>
2016-11-26 21:04:10 +0300

    Initial Commit

---
+++ HomeWork1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by Алексей Малашенков on 26.11.16.
+//  Copyright © 2016 Алексей Малашенков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
